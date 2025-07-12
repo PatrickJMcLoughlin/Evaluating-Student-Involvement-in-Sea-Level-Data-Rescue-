5ab77866dc1785207d992a5f6931b40c42fb0aa8
PatrickJMcLoughlin <patrick.mcloughlin.2014@mumail.ie>
2025-07-12 08:42:26 +0100

    Update Generate_Dun_Laoghaire_Marigram_Training_Images.R

--- Code/Generate_Dun_Laoghaire_Marigram_Training_Images.R
+++ Code/Generate_Dun_Laoghaire_Marigram_Training_Images.R
@@ -29,7 +29,7 @@ rm(list=ls())
 # 3. Load and Format Dún Laoghaire Data
 # ==========================================
 # **USER INSTRUCTION**: The data file '1925_DL_m_ODM.csv' should be located in the 'Data_Training' folder.
-# Ensure that the file '1925_DL_m_ODM.csv' is available in the "Excel_Data/Dún_Laoghaire" subfolder on GitHub, or 
+# Ensure that the file '1925_DL_m_ODM.csv' is available in the "Excel_Data/Dún_LaoghaireTraining_Data/" subfolder on GitHub, or 
 # place it in the appropriate directory relative to your working directory.
 
 # Example folder structure:
